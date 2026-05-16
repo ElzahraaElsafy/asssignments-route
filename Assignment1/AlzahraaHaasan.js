28f994c18f789f66ff725375aa1d343fa8d8e4aa
Alzahraa <elzahraaelsafy5@gmail.com>
2026-05-16 14:06:34 +0300

    init my repo

---
+++ Assignment1/AlzahraaHaasan.js
@@ -0,0 +1,110 @@
+// let A="123";
+// let B=Number("123")+7;
+// console.log(B);
+// console.log("========================================");
+// let x=0;
+// if(x == false){
+//     console.log("Invalid Value");
+// };
+// console.log("=========================================");
+// for(let i=0;i<10;i++){
+//     if(i%2==0){
+//         continue;
+//     }else{
+//         console.log(i);
+//     }};
+//     console.log("============================================");
+//     let arr=[1,2,3,4,5,6];
+//     arr.filter((n,index,arr) => {
+//         if(n%2==0){
+//             console.log(n);
+//         }
+//     });
+// console.log("================================================");
+// const merarr=(array1,array2)=>{
+//     return[...array1,...array2];
+// }
+// let array1=[1,2,3];
+// let array2=[4,6,7];
+// console.log(merarr(array1,array2));
+// console.log("==============================");
+// let day=3;
+// switch(day){
+//     case 1:
+//         console.log("Saturday");
+//         break;
+//     case 2:
+//         consolr.log("sunday");
+//         break;
+//     case 3:
+//         console.log("Monday");
+//         break;
+//     case 4:
+//         console.log("Tuesday");
+//         break;
+//     case 5:
+//          console.log("Wedenseday");
+//         break;
+//     case 6:
+//         console.log("Thursday");
+//         break;
+//     case 7:
+//         console.log("Friday");
+//         break;
+//            console.log("Invalid day");
+//             default:
+// }
+// consolelog("============================");
+// const StringLength=(arr)=>{
+//     return arr.map(str=>str.length);
+// }
+// const string=["a","ab","abc"];
+// console.log(StringLength(string));
+// console.log("=============================");
+// function reminder(a,b,c){
+//     if(a%b==0 && a%c==0){
+//         console.log("Divisible by both");
+//     }
+// }
+// reminder(15,3,5);
+// console.log("============================");
+// let square=(num)=>{
+//     return num*num;
+// }
+// console.log(square(7));
+// console.log("=======================");
+// let obj={
+//     name: "john",
+//     age: 70,
+//     city: "Cairo",
+//     print:function(){
+//         console.log(`${this.name} is ${this.age} years old and lives in  ${this.city}`);
+//     }
+// }
+// obj.print();
+// let arr=[1,2,3,4,5,6,7];
+// let res=arr.reduce((acc,n,index) => {
+//     return acc+n ;
+// },0)
+// console.log({res});
+// console.log("===============================");
+// const result = setTimeout(function(){
+// const pro =Promise.resolve("Connected");
+//     console.log(pro);
+// },3000);
+// console.log(result);
+// console.log("============================");
+// const TheMaxNum= (arr) => {
+//     return Math.max(...arr);
+// };
+// const numbers = [1, 3, 7, 2, 4];
+// console.log(TheMaxNum(numbers)); 
+// console.log("=======================");
+// const getKey = (obj) => {
+//     return Object.keys(obj);
+// };
+// const user = { name: "John", age: 30,city :'Cairo' };
+// console.log(getKey(user));
+// console.log("==============================");
+str="The quick brown fox";
+console.log(str.split(" "));
